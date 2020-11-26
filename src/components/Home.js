6e745a9d0afe9b14bb4a3f79e83b9f0e41c2b057
Tom Hewitt <tommhewitt@gmail.com>
2020-11-26 21:12:21 +0000

    chore(#1): add a logout button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { getUserData } from '../util/localStorage';
+import LogoutButton from './Buttons/Logout';
 
 const Home = () => (
   <>
     <div>Home</div>
+    <LogoutButton />
     <div>
       {JSON.stringify(getUserData())}
     </div>
--- src/util/localStorage.js
+++ src/util/localStorage.js
@@ -1,9 +1,5 @@
-export const saveUserData = (user) => {
-  localStorage.setItem('user', JSON.stringify(user));
-};
+export const saveUserData = (user) => localStorage.setItem('user', JSON.stringify(user));
 
 export const getUserData = () => JSON.parse(localStorage.getItem('user'));
 
-export const clearUserData = () => {
-  localStorage.removeItem('user');
-};
+export const clearUserData = () => localStorage.removeItem('user');
