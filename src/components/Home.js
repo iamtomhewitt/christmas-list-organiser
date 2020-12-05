5bfe5517bd6a96e7cd367fae1263e902b188fcbf
Tom Hewitt <tommhewitt@gmail.com>
2020-12-05 20:00:08 +0000

    chore(#36): add version to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { getUserData } from '../util/localStorage';
 import ChristmasList from './ChristmasList/ChristmasList';
+import { version } from '../../package.json';
+import './Home.scss';
 
 export const Home = (props) => {
   const { email } = props.location || getUserData();
 
   return (
-    <ChristmasList email={email} />
+    <>
+      <ChristmasList email={email} />
+      <div className="version">version {version}</div>
+    </>
   );
 };
 
