8bfcf4f560ddb2ed8b3f60452f901e197300e730
Tom Hewitt <tommhewitt@gmail.com>
2020-12-05 18:36:19 +0000

    chore(#36): use env variable for backend url

--- src/api/account.js
+++ src/api/account.js
@@ -1,8 +1,11 @@
 import { get, post } from '../util/http';
 import URL from '../config';
 
+console.log('env', process.env.BACKEND_URL)
 const host = process.env.BACKEND_URL || 'http://localhost:8080'
+console.log('Host', host)
 const url = `${host}/account`;
+console.log('url', url)
 
 export const getAccount = (email) => get(`${url}?email=${email}`);
 
