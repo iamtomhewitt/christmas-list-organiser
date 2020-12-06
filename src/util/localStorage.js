d5bc7ba352f95c686a111d3ce0f414d7e8de072f
Tom Hewitt <tommhewitt@gmail.com>
2020-12-06 22:35:58 +0000

    chore(#24): use session storage instead of local storage

--- src/util/localStorage.js
+++ src/util/localStorage.js
@@ -1,5 +1,5 @@
-export const saveUserData = (user) => localStorage.setItem('user', JSON.stringify(user));
+export const saveUserData = (user) => window.sessionStorage.setItem('user', JSON.stringify(user));
 
-export const getUserData = () => JSON.parse(localStorage.getItem('user'));
+export const getUserData = () => JSON.parse(window.sessionStorage.getItem('user'));
 
-export const clearUserData = () => localStorage.removeItem('user');
+export const clearUserData = () => window.sessionStorage.removeItem('user');
